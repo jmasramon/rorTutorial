d3a2e9d5e73648bbe3bb62d09d58e08de9f13694
Jordi Masramon <jordi.masramon@gmail.com>
2013-03-29 21:25:10 +0100

    Afegeixo recursos i actualitzo

---
+++ test/unit/micropost_test.rb
@@ -0,0 +1,22 @@
+require 'test_helper'
+
+class MicropostTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+  test "content should be longer than 10" do
+    mp = Micropost.new(:content => "a"*3, :user_id => 1)
+    assert !mp.save
+    # puts mp.errors.full_messages
+    assert mp.errors.full_messages == ["Content is too short (minimum is 10 characters)"]
+  end
+
+  test "content should be shorter than 140" do
+    mp = Micropost.new(:content => "a"*150, :user_id => 1)
+    assert !mp.save
+    # puts mp.errors.full_messages
+    assert mp.errors.full_messages == ["Content is too long (maximum is 140 characters)"]
+  end
+
+
+end

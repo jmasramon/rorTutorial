5532dbd031515d8ad60b497bd9686618eac448e5
Jordi Masramon <jordi.masramon@gmail.com>
2013-03-31 10:07:16 +0200

    Finish static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -12,18 +12,34 @@ describe "StaticPages" do
 
     it "should <h></h>ave the content 'Sample App' in home" do
       visit '/static_pages/home'
-      page.should have_content('Sample App')
-    end
+
+      page.should have_selector('h1',
+                                :text => "Sample App")
+
+      page.should have_selector('title',
+                                :text => "Ruby on Rails Tutorial Sample App | Home")
+
+    end
 
     it "should have the content 'Help' in help" do
       visit '/static_pages/help'
       page.should have_content('Help')
+      page.should have_selector('title',
+
+                                :text => "Ruby on Rails Tutorial Sample App | Help")
+
+
     end
 
     describe "About page" do
       it "should have the content 'About Us'" do
         visit '/static_pages/about'
         page.should have_content('About Us')
+      page.should have_selector('title',
+
+                                :text => "Ruby on Rails Tutorial Sample App | About")
+
+
       end
     end
   end
